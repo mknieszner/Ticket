1d1d972e23a596ac38a5485da8f570051b3b593f
mknieszner@gmail.com <maciosza11>
2018-07-07 00:40:40 +0200

    init

--- src/main/resources/static/polyfills.js
+++ src/main/resources/static/polyfills.js
@@ -5454,7 +5454,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Projekty\Ticket\frontend\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\Projekty\my-jira\frontend\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
