2165e50b1083ea776fa18b61a8708bd912638cf6
mknieszner@gmail.com <maciosza11>
2018-07-06 02:06:05 +0200

    update

--- src/main/java/ticketproject/app/crud/domain/entities/authorization/User.java
+++ src/main/java/ticketproject/app/crud/domain/entities/authorization/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cascade;
 import ticketproject.app.crud.domain.entities.Task;
 
 
@@ -22,69 +23,71 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 public class User implements Serializable {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  @Column(name = "USERNAME", unique = true)
-  private String username;
-  private String firstName;
-  private String lastName;
-  private String email;
-  private String password;
-  private boolean enabled;
-  @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER, cascade=CascadeType.ALL)
-  Set<Role> roles = new HashSet<>();
-  @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER, cascade=CascadeType.ALL)
-  List<Task> tasks = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "USERNAME", unique = true)
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
+    @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Cascade(value = org.hibernate.annotations.CascadeType.ALL)
+    Set<Role> roles = new HashSet<>();
+    @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Cascade(value = org.hibernate.annotations.CascadeType.ALL)
+    List<Task> tasks = new ArrayList<>();
 
-  public User(final String username,
-              final String firstName,
-              final String lastName,
-              final String email,
-              final String password,
-              final boolean enabled,
-              final List<Task> tasks) {
-    this.username = username;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-    this.enabled = enabled;
-    this.tasks.addAll(tasks);
-  }
+    public User(final String username,
+                final String firstName,
+                final String lastName,
+                final String email,
+                final String password,
+                final boolean enabled,
+                final List<Task> tasks) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+        this.tasks.addAll(tasks);
+    }
 
-  public User(final String username) {
+    public User(final String username) {
 
-  }
+    }
 
-  public void addRole(Role role) {
-    role.getUsers().add(this);
-    roles.add(role);
-  }
+    public void addRole(Role role) {
+        role.getUsers().add(this);
+        roles.add(role);
+    }
 
-  public void removeRoleWARN(Role role) {
-    roles.remove(role);
-    role.getUsers().remove(this);
-  }
+    public void removeRoleWARN(Role role) {
+        roles.remove(role);
+        role.getUsers().remove(this);
+    }
 
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    final User user = (User) o;
+        final User user = (User) o;
 
-    return username.equals(user.username);
-  }
+        return username.equals(user.username);
+    }
 
-  @Override
-  public int hashCode() {
-    return username.hashCode();
-  }
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
 
-  @Override
-  public String toString() {
-    return "User{" + "id=" + id + ", username='" + username + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" +
-        email + '\'' + ", password='" + password + '\'' + ", enabled=" + enabled + ", roles=" + roles.stream().map(Role::getName).collect(Collectors.toList()) + '}';
-  }
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username='" + username + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" +
+                email + '\'' + ", password='" + password + '\'' + ", enabled=" + enabled + ", roles=" + roles.stream().map(Role::getName).collect(Collectors.toList()) + '}';
+    }
 }
