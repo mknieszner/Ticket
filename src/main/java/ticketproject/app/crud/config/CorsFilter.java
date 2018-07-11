a7c3bab399bafff8701797f6fe158c034e553b25
mknieszner@gmail.com <maciosza11>
2018-07-11 20:34:45 +0200

    update

--- src/main/java/ticketproject/app/crud/config/CorsFilter.java
+++ src/main/java/ticketproject/app/crud/config/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter implements Filter {
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     final HttpServletResponse response = (HttpServletResponse) res;
-    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Origin", "https://my-jira.herokuapp.com/");
     response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
     response.setHeader("Access-Control-Allow-Credentials", "true");
